cdb50ee34061542a10ea425623b1fe95c5fbedbd
Martijn Boers <martijn.s.boers@gmail.com>
2015-09-15 22:59:57 +0200

    Vergeten toe te voegen

--- js/main.js
+++ js/main.js
@@ -1,46 +1,24 @@
 $(document).ready(function() {
-    var activeSystemClass = $('.list-group-item.active');
 
-    //something is entered in search form
-    $('#system-search').keyup( function() {
-       var that = this;
-        // affect all table rows on in systems table
-        var tableBody = $('.table-list-search tbody');
-        var tableRowsClass = $('.table-list-search tbody tr');
-        $('.search-sf').remove();
-        tableRowsClass.each( function(i, val) {
-        
-            //Lower text for case insensitive
-            var rowText = $(val).text().toLowerCase();
-            var inputText = $(that).val().toLowerCase();
-            if(inputText != '')
-            {
-                $('.search-query-sf').remove();
-                tableBody.prepend('<tr class="search-query-sf"><td colspan="6"><strong>Searching for: "'
-                    + $(that).val()
-                    + '"</strong></td></tr>');
-            }
-            else
-            {
-                $('.search-query-sf').remove();
-            }
+	// this is the id of the form
+	var activeSystemClass = $('.list-group-item.active');
+	$("#formSub").submit(function() {
+		var tableBody = $('.table-list-search tbody');
+		var tableRowsClass = $('.table-list-search tbody tr');
+		var url = "backend/api.php"; // the script where you handle
+		// the form input.
+		$.ajax({
+			type : "POST",
+			url : url,
+			data : $("#formSub").serialize(), // serializes the form's
+			// elements.
+			success : function(data) {
+				tableBody.empty();
+				tableBody.prepend(data);
+				$(".empty").hide();
+			}
+		});
 
-            if( rowText.indexOf( inputText ) == -1 )
-            {
-                //hide rows
-                tableRowsClass.eq(i).hide();
-                
-            }
-            else
-            {
-                $('.search-sf').remove();
-                tableRowsClass.eq(i).show();
-            }
-        });
-        //all tr elements are hidden
-        if(tableRowsClass.children(':visible').length == 0)
-        {
-            tableBody.append('<tr class="search-sf"><td class="text-muted" colspan="6">No entries found.</td></tr>');
-        }
-    });
+		return false; // avoid to execute the actual submit of the form.
+	});
 });
